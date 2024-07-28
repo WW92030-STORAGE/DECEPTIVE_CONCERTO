03d1c07104d7de19044589490fe0d34531f415e7
NormalExisting <WILLIAMSTORAGE69@gmail.com>
2024-07-27 23:01:37 -0500

    traps update

--- entities/Trap.gd
+++ entities/Trap.gd
@@ -16,7 +16,7 @@ func _process(delta):
 		$MeshInstance3D.get_active_material(0).emission_enabled = true
 	else:
 		$MeshInstance3D.get_active_material(0).albedo_color = Color(0.5, 0.5, 0.5)
-		$MeshInstance3D.get_active_material(0).emission_enabled = false
+		$MeshInstance3D.get_active_material(0).emission_enabled = true
 	
 	$Laser.visible = TIME_DISP >= ACTIVATE
 	
